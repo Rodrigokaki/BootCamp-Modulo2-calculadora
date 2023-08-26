7eff0542ee281eb8d4c771be55498dcca0d5a7f8
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2023-08-26 12:11:58 -0300

    closes #4 - operacoes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-function add(){
+function calc(operation){
     var num1 = +document.getElementById("num1").value;
     var num2 = +document.getElementById("num2").value;
+    var result = 0;
 
-    var result = num1 + num2;
+    switch(operation){
+        case '+' : result = num1 + num2; break;
+        case '-' : result = num1 - num2; break;
+        case '*' : result = num1 * num2; break;
+        case '/' : result = num1 / num2; break;
+    }
     
-    document.getElementById("result").value = result.toFixed(2);
+    document.getElementById("result").value = result;
 }
