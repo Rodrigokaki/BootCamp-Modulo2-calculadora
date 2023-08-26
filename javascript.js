b924dd9cfec3a76338600060f300f50d721a8b6c
rodrigo kakiuchi de souza <rodrigo.kakiuchi2010@gmail.com>
2023-08-26 11:35:43 -0300

    closes #3 - botao somar

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function add(){
+    var num1 = +document.getElementById("num1").value;
+    var num2 = +document.getElementById("num2").value;
+
+    var result = num1 + num2;
+    
+    document.getElementById("result").value = result.toFixed(2);
+}
